e103312f5a91ef9868c71431647cfac656990e6e
Tassilo Koller <tassilok@gmx.de>
2015-01-26 21:57:08 +0100

    Result-Class for webservices

---
+++ OntWeb/OntWeb/WebServiceResult.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using OntologyClasses.BaseClasses;
+
+namespace OntWeb
+{
+    public class WebServiceResult
+    {
+        public clsOntologyItem Result { get; set;  }
+        public List<clsOntologyItem> OntologyItems { get; set; }
+        public List<clsClassAtt> ClassAttributes { get; set; }
+        public List<clsClassRel> ClassRelations { get; set;  }
+        public List<clsObjectAtt> ObjectAttributes { get; set; }
+        public List<clsObjectRel> ObjectRelations { get; set; }
+    }
+}
